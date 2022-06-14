0f057fb4d024f3d75497e6b841aa225386eeee03
Ralph M <ralymuhif@yahoo.fr>
2022-06-14 12:48:47 -0400

    Add Cylinder and Circle classes

---
+++ oop_exercises/cylinder/TestCylinder.java
@@ -0,0 +1,37 @@
+package oop_exercises.cylinder;
+
+public class TestCylinder { // save as "TestCylinder.java"
+   public static void main(String[] args) {
+      // Declare and allocate a new instance of cylinder
+      // with default color, radius, and height
+      Cylinder c1 = new Cylinder();
+      System.out.println("Cylinder:"
+            + " radius=" + c1.getRadius()
+            + " height=" + c1.getHeight()
+            + " base area=" + c1.getArea()
+            + " volume=" + c1.getVolume()
+            + " color=" + c1.getColor());
+
+      // Declare and allocate a new instance of cylinder
+      // specifying height, with default color and radius
+      Cylinder c2 = new Cylinder(15.0, "Blue", 10.0);
+      System.out.println("Cylinder:"
+            + " radius=" + c2.getRadius()
+            + " height=" + c2.getHeight()
+            + " base area=" + c2.getArea()
+            + " volume=" + c2.getVolume()
+            + " color=" + c2.getColor());
+
+      // Declare and allocate a new instance of cylinder
+      // specifying radius and height, with default color
+      Cylinder c3 = new Cylinder(2.0, 10.0);
+      System.out.println("Cylinder:"
+            + " radius=" + c3.getRadius()
+            + " height=" + c3.getHeight()
+            + " base area=" + c3.getArea()
+            + " volume=" + c3.getVolume()
+            + " color=" + c3.getColor());
+
+      System.out.println(c3);
+   }
+}
